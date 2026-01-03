a763d4b44764dd26f28e07c9c30944521ecb20c5
Aditya Pahane <adityapahane@gmail.com>
2026-01-03 16:12:36 +0530

    feat: add PostCSS

--- website/vite.config.js
+++ website/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
+    plugins: [
     react(),
     tailwindcss(),
   ],
+  css: {
+    transformer: 'postcss',
+  },
 })
